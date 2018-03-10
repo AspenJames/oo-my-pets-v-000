b2b7e7140ce95d14a7c59c567acea3a7c6b38826
Aspen James <aspenjames@tqca.org>
2018-03-10 01:08:50 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,8 +54,8 @@ class Owner
   end
 
   def sell_pets
-    self.pets.each do |pets|
-      pets.each{|pet| pet.mood = "nervous"}
+    self.pets.each do |pet_type|
+      pet_type.each{|pet| pet.mood = "nervous"}
     end
   end
 
