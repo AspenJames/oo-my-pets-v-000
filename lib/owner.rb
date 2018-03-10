92f43b7a2a7322cd4d67b2f2aa23353950102365
Aspen James <aspenjames@tqca.org>
2018-03-10 01:05:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,5 +41,17 @@ class Owner
     self.pets[:dogs] << d
   end
 
+  def walk_dogs
+    self.pets[:dogs].each{|d| d.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].each{|c| c.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each{|f| f.mood = "happy"}
+  end
+
 
 end
