fa0166d9eaf04d5a52d3650ed570e326538ecd6c
Aspen James <aspenjames@tqca.org>
2018-03-10 00:56:14 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
+  attr_reader :species, :name
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    
+  end
+
+  def pets
+    
+  end
 
 end
